f77d11ff31c4c66386370025ccc16f8b8e7e723f
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-12-13 21:34:31 -0300

    :sparkles: feat: Adição da Pagina de Login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,27 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import Link from "next/link";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function HomePag() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <main className="flex flex-col min-h-screen bg-gray-300">
+        <h2 className="flex justify-center pt-20 text-5xl text-gray-700">IquanafiX</h2>
+        <section className="flex justify-center pt-5">
+            <DotLottieReact src="https://lottie.host/4c6ab2dc-ded9-4eaf-83f5-ce62f0d3d8f0/2qexxLLi47.lottie" loop autoplay className="flex w-2xl"/>
+        </section>
+        <h2 className="text-center pt-5 text-2xl text-gray-700">Seja bem Vindo!</h2>
+        <p className="text-center text-gray-700 pt-20">
+          Tenha acesso a centenas de trabalho diariamente. Faça seu negócio crecer e 
+          consiga novos clientes pertos da sua casa!
+        </p>
+
+        <section className="flex flex-col justify-center items-center pt-9 gap-2.5 bg-gray-300">
+          <button className="bg-blue-700 text-gray-300 p-2 w-80 mr-3.5 ml-3.5">Criar conta</button>
+          <button className="text-blue-700 p-2 border border-blue-600 w-80 mr-3.5 ml-3.5">Já tenho conta</button>
+        </section>
       </main>
-    </div>
-  );
+    </>
+  )
 }
